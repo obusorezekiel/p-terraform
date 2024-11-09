ff83ce5a346ab0463b3959d0f12edc1ebaf34c57
Ezekiel Umesi <obusorezekiel@gmail.com>
2024-11-09 09:50:59 +0100

    modified s3 and dynamodb output

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ locals {
 }
 
 module "s3" {
-  source      = "./modules/s3"
+  source      = "./modules/s3_bucket"
   bucket_name = "my-terraform-state-bucket"
   kms_key_arn = module.kms.kms_key_arn
 }
 
 module "dynamodb" {
-  source     = "./modules/dynamodb"
+  source     = "./modules/dynamodb_table"
   table_name = "terraform-locks"
 }
 
